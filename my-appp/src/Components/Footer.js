d5ec36e7222605596d8055a2ed78af178ee356d7
Siddharth0369 <siddharthravikumar3@gmail.com>
2022-07-12 13:35:24 +0530

    sid's commit

--- my-appp/src/Components/Footer.js
+++ my-appp/src/Components/Footer.js
@@ -4,7 +4,7 @@ export class Footer extends Component {
   render() {
     return (
       <div className='footer'>
-        <a href="https://instagram.com/sanju_ch_8?igshid=YmMyMTA2M2Y=" style={{color:"#FF5B00"}}>©2022 Purview</a>
+        <a href="https://instagram.com/sanju_ch_8?igshid=YmMyMTA2M2Y=" style={{color:"#FF5B00"}}>©2022 Siddharth</a>
       </div>
     )
   }
