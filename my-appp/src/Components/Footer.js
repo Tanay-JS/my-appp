c940723a16f7424babd194a2f7812298c175d57a
Sanjuboi-24 <s_chimakurthy@yahoo.in>
2022-07-12 13:06:07 +0530

    c12

--- my-appp/src/Components/Footer.js
+++ my-appp/src/Components/Footer.js
@@ -4,11 +4,7 @@ export class Footer extends Component {
   render() {
     return (
       <div className='footer'>
-<<<<<<< HEAD
-        <a href="https://instagram.com/sanju_ch_8?igshid=YmMyMTA2M2Y=" style={{color:"#FF5B00"}}>©2022 Purview</a>
-=======
         <a href="https://instagram.com/sanju_ch_8?igshid=YmMyMTA2M2Y=" style={{color:"#FF5B00"}}>©2024 praveen</a>
->>>>>>> 7f0e77baadd63e30365e9e70ec5a2ea8c8bf4845
       </div>
     )
   }
